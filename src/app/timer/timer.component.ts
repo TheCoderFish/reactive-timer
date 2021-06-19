f8d32f7317b1b1228934d2736bb278b198bf5ab4
Nurali Khoja <nurali@tradexa.co.in>
2021-06-19 14:32:43 +0530

    complete timer functionality

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, merge, NEVER, Observable, Subject } from 'rxjs';
-import { map, mapTo, pluck, scan, startWith, switchMap, tap } from 'rxjs/operators';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, interval, merge, NEVER, Observable, Subject } from 'rxjs';
+import { map, mapTo, pluck, scan, skip, startWith, switchMap, tap } from 'rxjs/operators';
 import { Modes, OutputTimeValue, TimerState, TimeValue, TimeValues } from '../models/timer-state';
 
 const SECONDS_IN_A_MINUTE = 60;
@@ -17,62 +17,61 @@ export class TimerComponent implements OnInit {
   startButtonClicked$ = new Subject();
   pauseButtonClicked$ = new Subject();
   resetButtonClicked$ = new Subject();
-  changeTimerMode$ = new Subject<Modes>();
+
+  currentMode = new BehaviorSubject(Modes.Pomodoro);
+  currentMode$ = this.currentMode.asObservable().pipe(
+    map<Modes, TimerState>((mode: Modes) => this.getTimerForMode(mode)))
 
   Modes = Modes;
 
   timer$: Observable<TimerState>;
   displayTimer$: Observable<OutputTimeValue>;
+  timerState$ = new Subject();
 
   // events
   events$: Observable<any>;
 
-  constructor() { }
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
 
     this.events$ = merge(
       this.startButtonClicked$.pipe(mapTo({ isTicking: true })),
       this.pauseButtonClicked$.pipe(mapTo({ isTicking: false })),
-      this.resetButtonClicked$.pipe(mapTo({ value: { minutes: 0, seconds: 0 } })),
-      this.changeTimerMode$.pipe(map<Modes, TimerState>((mode: Modes) => {
-        let value: TimeValue;
-        switch (mode) {
-          case Modes.Pomodoro:
-            value = { minutes: TimeValues.Pomodoro.minutes, seconds: TimeValues.Pomodoro.seconds }
-            break;
-          case Modes.ShortBreak:
-            value = { minutes: TimeValues.ShortBreak.minutes, seconds: TimeValues.ShortBreak.seconds }
-            break;
-          case Modes.LongBreak:
-            value = { minutes: TimeValues.LongBreak.minutes, seconds: TimeValues.LongBreak.seconds }
-            break;
-          default:
-            value = { minutes: TimeValues.Pomodoro.minutes, seconds: TimeValues.Pomodoro.seconds }
-            break;
-        }
-        return ({ value });
-      }))
+      this.resetButtonClicked$.pipe(map(() => {
+        const value = this.getTimerForMode(this.currentMode.value)
+        return ({ ...value, isTicking: false })
+      })),
+      this.currentMode$.pipe(
+        skip(1),
+        tap(() => this.startButtonClicked$.next())
+      )
     )
 
     // create the timer with the initial state
     this.timer$ = this.events$.pipe(
       startWith({ isTicking: false, value: { minutes: TimeValues.Pomodoro.minutes, seconds: TimeValues.Pomodoro.seconds } }),
       scan((state: TimerState, curr): TimerState => ({ ...state, ...curr }), {}),
-      /* tap(console.log), */
       // create the actual timer by switching from base
+      tap(state => this.setTimerState(state)),
       switchMap((state: TimerState) =>
         state.isTicking ? interval(1000).pipe(
           map(tick => this.updateTimerStateOnTick(state)),
-          tap((_state: TimerState) => state.value = _state.value),
+          tap((_state: TimerState) => {
+            state.value = _state.value;
+            this.setTimerState(state);
+          })
         ) : NEVER),
     )
 
-    this.displayTimer$ = this.timer$.pipe(
-      startWith({ isTicking: false, value: { minutes: TimeValues.Pomodoro.minutes, seconds: TimeValues.Pomodoro.seconds } }),
+    this.displayTimer$ = this.timerState$.pipe(
       pluck('value'),
       map(((value: TimeValue) => ({ minutes: this.padNumber(value.minutes), seconds: this.padNumber(value.seconds) })))
     )
+
+    this.timer$.subscribe();
   }
 
   private padNumber(num: number) {
@@ -95,6 +94,34 @@ export class TimerComponent implements OnInit {
 
     const value = ({ minutes: _minutes, seconds: _seconds });
     return ({ ...currentState, value })
+  }
+
+  private setTimerState(state: TimerState) {
+    this.timerState$.next(state);
+    this.cdr.detectChanges();
+  }
+
+  private getTimerForMode(mode: Modes) {
+    let value: TimeValue;
+    switch (mode) {
+      case Modes.Pomodoro:
+        value = { minutes: TimeValues.Pomodoro.minutes, seconds: TimeValues.Pomodoro.seconds }
+        break;
+      case Modes.ShortBreak:
+        value = { minutes: TimeValues.ShortBreak.minutes, seconds: TimeValues.ShortBreak.seconds }
+        break;
+      case Modes.LongBreak:
+        value = { minutes: TimeValues.LongBreak.minutes, seconds: TimeValues.LongBreak.seconds }
+        break;
+      default:
+        value = { minutes: TimeValues.Pomodoro.minutes, seconds: TimeValues.Pomodoro.seconds }
+        break;
+    }
+    return ({ value });
+  }
+
+  setMode(mode: Modes) {
+    this.currentMode.next(mode);
   }
 
 }
